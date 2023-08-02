833365ee038eaa6b221c23d610a736d67e026235
Ricardo <ricoledan@gmail.com>
2023-08-02 16:18:17 -0400

    misc updates

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 
 🔨 Companion repo to "The Art of LangSmithing" by Ricardo Ledan
 
+## Tools
+
+* [Pyenv](https://github.com/pyenv/pyenv) 
+* [Serverless Framework](https://www.serverless.com/)
+
 ## Commands
 
 Install Python dependencies 
 
 ```bash
 pip install -r requirements.txt
+```
+
+Activate your local Python virtual environment (I personally use pyenv)
+
+```bash
+pyenv activate ENV_NAME
 ```
 
 Run the serverless function locally
--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
 import json
+from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
@@ -9,13 +9,12 @@ os.environ["LANGCHAIN_TRACING_V2"] = os.getenv('LANGCHAIN_TRACING_V2')
 os.environ["LANGCHAIN_ENDPOINT"] = os.getenv('LANGCHAIN_ENDPOINT')
 os.environ["LANGCHAIN_API_KEY"] = os.getenv('LANGCHAIN_API_KEY')
 os.environ["LANGCHAIN_PROJECT"] = os.getenv('LANGCHAIN_PROJECT')
-
 os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
 
 def chat(event, context):
-    llm = ChatOpenAI()
 
-    body = {"bot": llm.predict("Tell me a fun fact about Curtis Jackson")}
+    llm = ChatOpenAI()
+    body = {"bot": llm.predict("Tell me about NYC Drill music")}
 
     return {"statusCode": 200, "body": json.dumps(body)}
