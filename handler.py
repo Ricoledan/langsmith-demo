7c0a5ddc2d8859b7894adfaf6046462c834880e4
Ricardo <ricoledan@gmail.com>
2023-08-02 17:26:09 -0400

    update env references

--- handler.py
+++ handler.py
@@ -5,15 +5,14 @@ from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"] = os.getenv('LANGCHAIN_TRACING_V2')
-os.environ["LANGCHAIN_ENDPOINT"] = os.getenv('LANGCHAIN_ENDPOINT')
-os.environ["LANGCHAIN_API_KEY"] = os.getenv('LANGCHAIN_API_KEY')
-os.environ["LANGCHAIN_PROJECT"] = os.getenv('LANGCHAIN_PROJECT')
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+os.getenv('LANGCHAIN_TRACING_V2')
+os.getenv('LANGCHAIN_ENDPOINT')
+os.getenv('LANGCHAIN_API_KEY')
+os.getenv('LANGCHAIN_PROJECT')
+os.getenv('OPENAI_API_KEY')
 
 
-def chat(event, context):
-
+def chat():
     llm = ChatOpenAI()
     body = {"bot": llm.predict("Tell me about NYC Drill music")}
 
